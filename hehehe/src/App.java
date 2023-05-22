a6732ba4e1383c5648afecf45459ba6d0fa185e4
Gabriel Schultz <88725975+SSSchultz@users.noreply.github.com>
2023-05-22 15:37:01 -0300

    Initial commit

---
+++ hehehe/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1, n2, r;
+        n1 = 2;
+        n2 = 4;
+
+        r =n1 + n2;
+
+        System.out.println("Hello, World! \n " + r);
+
+    }
+}
